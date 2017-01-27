1e43c752c3dbb8663da2f9db869569d7a3557886
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-01-27 08:33:08 +0700

    perbaikan di rails 5 butuh deklarasi inverse_of di model associations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
 
-  has_many  :ingredients
-  has_many  :directions
+  has_many :ingredients, inverse_of: :recipe
+  has_many :directions, inverse_of: :recipe
 
   accepts_nested_attributes_for :ingredients,
                                  reject_if: proc { |attributes| attributes['name'].blank? },
